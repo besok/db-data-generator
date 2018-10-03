aa6eb9fba5ca0829bcb9a5e33da059141cc5ab55
Boris Zhguchev <zhguchev@gmail.com>
2018-10-03 14:54:49 +0300

    add test

--- src/main/java/ru/gpb/utils/db/data/generator/worker/Action.java
+++ src/main/java/ru/gpb/utils/db/data/generator/worker/Action.java
@@ -1,48 +1,77 @@
 package ru.gpb.utils.db.data.generator.worker;
 
+import java.util.Random;
 import java.util.concurrent.atomic.AtomicInteger;
 import java.util.concurrent.atomic.AtomicLong;
 
 /**
- *
  * Action class for changing output generated value.
  * For more detailed info, please, @see {@link ComplexPlainTypeGenerator}
- *
+ * <p>
  * This interface is SAM interface.
  *
  * @param <V> generated value type.
- *
- * Created by Boris Zhguchev on 01/10/2018
+ *            <p>
+ *            Created by Boris Zhguchev on 01/10/2018
  */
 public interface Action<V> {
 
   /**
-   * method for changing or modifying generated value.
-   * @param oldValue generated value from default generator @see {@link DefaultPlainTypeGenerator#generate(Class, MetaData.Column)}
-   * @return new value or modified oldValue
-   *
-   * */
-  V process(V oldValue);
+   * Simplify template ops.
+   */
+
+  static <V> Action<V> CONST(V v) {
+	return oldValue -> v;
+  }
+  static <V> Action<V> OLD() {
+	return oldValue -> oldValue;
+  }
+  static Action<Integer> INCREMENT_I(int startVal) {
+	return new Action<Integer>() {
+	  private AtomicInteger delegate = new AtomicInteger(startVal);
+
+	  @Override
+	  public Integer process(Integer oldValue) {
+		return delegate.incrementAndGet();
+	  }
+	};
+  }
+  static Action<Long> INCREMENT_L(int startVal) {
+	return new Action<Long>() {
+	  private AtomicLong delegate = new AtomicLong(startVal);
+
+	  @Override
+	  public Long process(Long oldValue) {
+		return delegate.incrementAndGet();
+	  }
+	};
+  }
+
+  @SuppressWarnings("unchecked")
+  static<V> Action<V> RANDOM(int bound) {
+	return (Action<V>) new Action<Object>() {
+	  private Random random = new Random();
+
+	  @Override
+	  public Object process(Object oldValue) {
+		switch (oldValue.getClass().getSimpleName()) {
+		  case "long":
+		  case "Long":
+		  case "int":
+		  case "Integer":
+			return random.nextInt(bound);
+		}
+		return oldValue;
+	  }
+	};
+  }
 
   /**
-   * Simplify template ops.
+   * method for changing or modifying generated value.
    *
-   * */
+   * @param oldValue generated value from default generator @see {@link DefaultPlainTypeGenerator#generate(Class, MetaData.Column)}
+   * @return new value or modified oldValue
+   */
+  V process(V oldValue);
 
-  static <V> Action<V> CONST(V v){return oldValue -> v ;}
-  static <V> Action<V> OLD(){ return oldValue -> oldValue;}
-  static Action<Integer> INCREMENT_I(int startVal){return new Action<Integer>() {
-    private AtomicInteger delegate = new AtomicInteger(startVal);
-    @Override
-    public Integer process(Integer oldValue) {
-      return delegate.incrementAndGet();
-    }
-  };}
-  static Action<Long> INCREMENT_L(int startVal){return new Action<Long>() {
-    private AtomicLong delegate = new AtomicLong(startVal);
-    @Override
-    public Long process(Long oldValue) {
-      return delegate.incrementAndGet();
-    }
-  };}
 }
